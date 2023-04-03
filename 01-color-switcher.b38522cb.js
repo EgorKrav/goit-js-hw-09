c2369513e007291c1d5aa3c2e1406f4d60d8a5e1
EgorKrav <123078885+EgorKrav@users.noreply.github.com>
2023-04-03 12:58:44 +0000

    Deploying to gh-pages from @ EgorKrav/goit-js-hw-09@e369367005128f01210d58bc0b398f5cbdd0a95b 🚀

---
+++ 01-color-switcher.b38522cb.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");let o;t.addEventListener("click",(function(){o=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.b38522cb.js.map
